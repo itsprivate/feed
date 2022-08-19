29f7bceb4587345cea15e656a87b77566d2f2a23
Owen Young <theowenyoung@gmail.com>
2022-08-20 03:47:36 +0800

    test

--- workflows/8-serve-archive-site.ts
+++ workflows/8-serve-archive-site.ts
@@ -88,89 +88,89 @@ export default function serveSite() {
     let itemsJson: ItemsJson | null = null;
     // const filePath = getArchivedFilePath(siteIdentifier, relativeItemsPath);
     const filePath = relativeItemsPath;
-    return Promise.resolve(
-      new Response("file path" + filePath, {
-        status: 200,
-      }),
-    );
-    // if (isDev()) {
-    //   // get file
-    //   try {
-    //     itemsJson = await readJSONFile(filePath);
-    //   } catch (_e) {
-    //     // 404
-    //     return Promise.resolve(
-    //       new Response("Not Found", {
-    //         status: 404,
-    //       }),
-    //     );
-    //   }
-    // } else {
-    //   // read from remote
-    //   const bucket = getArchivedBucketName();
-    //   console.log("bucket", bucket);
-    //   const s3Bucket = await getArchiveS3Bucket(bucket);
-    //   const s3Object = await s3Bucket.getObject(filePath);
-    //   if (s3Object) {
-    //     const { body } = s3Object;
-    //     itemsJson = await new Response(body).json();
-    //   }
-    // }
-    // if (itemsJson) {
-    //   const feedjson = itemsToFeed(
-    //     relativeItemsPath,
-    //     itemsJson,
-    //     siteIdentifier,
-    //     language.code,
-    //     config,
-    //     {
-    //       isArchive: true,
-    //     },
-    //   );
-    //   let type = "feedjson";
-    //   // TODO: write temp json
-    //   // await writeJSONFile("temp.json", feedjson);
-    //   if (type === "feedjson") {
-    //     return Promise.resolve(
-    //       new Response(JSON.stringify(feedjson), {
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //         },
-    //       }),
-    //     );
-    //   } else if (type === "rssxml") {
-    //     // @ts-ignore: npm module
-    //     const rssOutput = jsonfeedToRSS(feedjson, {
-    //       language: feedjson.language,
-    //     });
-    //     return Promise.resolve(
-    //       new Response(rssOutput, {
-    //         headers: {
-    //           "Content-Type": "application/xml",
-    //         },
-    //       }),
-    //     );
-    //   }
-    //   const html = feedToHTML(feedjson, config);
-    //   return new Response(html, {
+    // return Promise.resolve(
+    //   new Response("file path" + filePath, {
     //     status: 200,
-    //     headers: {
-    //       "content-type": "text/html; charset=utf-8",
-    //     },
-    //   });
-    // } else {
-    //   return Promise.resolve(
-    //     new Response("Not Found", {
-    //       status: 404,
-    //     }),
-    //   );
-    // }
+    //   }),
+    // );
+    if (isDev()) {
+      // get file
+      try {
+        itemsJson = await readJSONFile(filePath);
+      } catch (_e) {
+        // 404
+        return Promise.resolve(
+          new Response("Not Found", {
+            status: 404,
+          }),
+        );
+      }
+    } else {
+      // read from remote
+      const bucket = getArchivedBucketName();
+      console.log("bucket", bucket);
+      const s3Bucket = await getArchiveS3Bucket(bucket);
+      const s3Object = await s3Bucket.getObject(filePath);
+      if (s3Object) {
+        const { body } = s3Object;
+        itemsJson = await new Response(body).json();
+      }
+    }
+    if (itemsJson) {
+      const feedjson = itemsToFeed(
+        relativeItemsPath,
+        itemsJson,
+        siteIdentifier,
+        language.code,
+        config,
+        {
+          isArchive: true,
+        },
+      );
+      // let type = "feedjson";
+      // TODO: write temp json
+      // await writeJSONFile("temp.json", feedjson);
+      if (type === "feedjson") {
+        return Promise.resolve(
+          new Response(JSON.stringify(feedjson), {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }),
+        );
+      } else if (type === "rssxml") {
+        // @ts-ignore: npm module
+        const rssOutput = jsonfeedToRSS(feedjson, {
+          language: feedjson.language,
+        });
+        return Promise.resolve(
+          new Response(rssOutput, {
+            headers: {
+              "Content-Type": "application/xml",
+            },
+          }),
+        );
+      }
+      const html = feedToHTML(feedjson, config);
+      return new Response(html, {
+        status: 200,
+        headers: {
+          "content-type": "text/html; charset=utf-8",
+        },
+      });
+    } else {
+      return Promise.resolve(
+        new Response("Not Found", {
+          status: 404,
+        }),
+      );
+    }
   };
   const port = Number(Deno.env.get("PORT") || 8000);
   log.info(
     `HTTP webserver running. Access it at: http://localhost:${port}/`,
   );
-  serve(handler);
+  serve(handler, { port });
 }
 
 if (import.meta.main) {
