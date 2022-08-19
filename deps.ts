61eeed1513ddcf9168bbd143847c9a13637fa313
Owen Young <theowenyoung@gmail.com>
2022-08-20 03:35:36 +0800

    fix site

--- deps.ts
+++ deps.ts
@@ -32,35 +32,36 @@ export {
 // third party modules
 
 // export { colors } from "https://deno.land/x/cliffy@v0.24.2/ansi/colors.ts";
-export { default as mustache } from "https://jspm.dev/mustache@4.2.0";
-export { default as OpenCC } from "https://jspm.dev/opencc-js@1.0.4";
-export { default as jsonfeedToAtom } from "https://jspm.dev/jsonfeed-to-atom@1.2.2";
-export { default as jsonfeedToRSS } from "https://jspm.dev/jsonfeed-to-rss@3.0.6";
-export { resize } from "https://deno.land/x/deno_image@0.0.4/mod.ts";
-export { config as dotenvConfig } from "https://deno.land/std@0.152.0/dotenv/mod.ts";
-export {
-  DOMParser,
-  Element,
-} from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
-export type {
-  DOMParserMimeType,
-} from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
-export { getMetadata } from "https://jspm.dev/page-metadata-parser@1.1.4";
-export { default as slug } from "https://jspm.dev/slug@6.0.0";
-export {
-  Browser,
-  default as puppeteer,
-  Page,
-} from "https://deno.land/x/puppeteer@14.1.1/mod.ts";
-export { default as staticFiles } from "https://deno.land/x/static_files@1.1.6/mod.ts";
+// export { default as mustache } from "https://jspm.dev/mustache@4.2.0";
+// export { default as OpenCC } from "https://jspm.dev/opencc-js@1.0.4";
+// export { default as jsonfeedToAtom } from "https://jspm.dev/jsonfeed-to-atom@1.2.2";
+// export { default as jsonfeedToRSS } from "https://jspm.dev/jsonfeed-to-rss@3.0.6";
+// export { resize } from "https://deno.land/x/deno_image@0.0.4/mod.ts";
+// export { config as dotenvConfig } from "https://deno.land/std@0.152.0/dotenv/mod.ts";
+// export {
+//   DOMParser,
+//   Element,
+// } from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
+// export type {
+//   DOMParserMimeType,
+// } from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
+// export { getMetadata } from "https://jspm.dev/page-metadata-parser@1.1.4";
+// export { default as slug } from "https://jspm.dev/slug@6.0.0";
 
-export {
-  GetObjectCommand,
-  HeadObjectCommand,
-  ListBucketsCommand,
-  ListObjectsV2Command,
-  PutObjectCommand,
-  S3Client,
-} from "https://deno.land/x/aws_sdk@v3.32.0-1/client-s3/mod.ts";
-export { getSignedUrl } from "https://deno.land/x/aws_sdk@v3.32.0-1/s3-request-presigner/mod.ts";
-export { S3, S3Bucket } from "https://deno.land/x/s3@0.5.0/mod.ts";
+// export { default as staticFiles } from "https://deno.land/x/static_files@1.1.6/mod.ts";
+
+// export {
+//   GetObjectCommand,
+//   HeadObjectCommand,
+//   ListBucketsCommand,
+//   ListObjectsV2Command,
+//   PutObjectCommand,
+//   S3Client,
+// } from "https://deno.land/x/aws_sdk@v3.32.0-1/client-s3/mod.ts";
+
+// export { S3, S3Bucket } from "https://deno.land/x/s3@0.5.0/mod.ts";
+// export {
+//   Browser,
+//   default as puppeteer,
+//   Page,
+// } from "https://deno.land/x/puppeteer@14.1.1/mod.ts";
