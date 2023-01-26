04fad431a950ee8f45a09317abec6f346b71b594
Owen Young <theowenyoung@gmail.com>
2023-01-26 18:55:30 +0800

    fix version

--- bad-deps.ts
+++ bad-deps.ts
@@ -3,6 +3,7 @@ export {
   default as puppeteer,
   Page,
 } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-export { default as OpenCC } from "https://jspm.dev/opencc-js@1.0.4";
+import * as OpenCC from "https://jspm.dev/opencc-js@1.0.5";
+export { OpenCC };
 export { resize } from "https://deno.land/x/deno_image@0.0.4/mod.ts";
 export { compress, decompress } from "./zip/mod.ts";
--- zip/deps.ts
+++ zip/deps.ts
@@ -1,2 +1,2 @@
-export { join } from "https://deno.land/std@0.133.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.133.0/fs/mod.ts";
+export { join } from "https://deno.land/std@0.174.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.174.0/fs/mod.ts";
