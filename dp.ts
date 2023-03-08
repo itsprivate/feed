f43dcf7a999d1b88b45932c6acde546ce15133dc
Owen Young <theowenyoung@gmail.com>
2023-03-09 07:36:16 +0800

    add test

--- dp.ts
+++ dp.ts
@@ -2,7 +2,32 @@ import { Deepl as DPro } from "https://esm.sh/immersive-translate@1.0.8";
 import { delay } from "./deps.ts";
 import { toZhHant } from "./to-zh-hant.ts";
 import { Language } from "./d/interface.ts";
+
+const langMap: [Language, string][] = [
+  ["auto", "auto"],
+  ["zh-CN", "ZH"],
+  ["zh-TW", "ZH"],
+  ["zh-Hans", "ZH"],
+  ["zh", "ZH"],
+  ["de", "DE"],
+  ["en", "EN"],
+  ["es", "ES"],
+  ["fr", "FR"],
+  ["it", "IT"],
+  ["ja", "JA"],
+  ["pt", "PT"],
+  ["ru", "RU"],
+  ["tr", "tr"],
+];
 export default class Translation {
+  /** Translator lang to custom lang */
+  private static readonly langMap = new Map(langMap);
+
+  /** Custom lang to translator lang */
+  private static readonly langMapReverse = new Map(
+    langMap.map(([translatorLang, lang]) => [lang, translatorLang]),
+  );
+
   constructor() {
   }
 
@@ -21,10 +46,23 @@ export default class Translation {
       )
     ) {
       await delay(20);
+      // @ts-ignore: it's ok
+      const sourceLanguageRemote = Translation.langMap.get(sourceLanguage) ||
+        sourceLanguage;
+      // @ts-ignore: it's ok
+      const targetLanguageRemote = Translation.langMap.get(targetLanguage) ||
+        targetLanguage;
+      console.log(
+        "sentence",
+        sentence,
+        sourceLanguageRemote,
+        targetLanguageRemote,
+      );
+
       const result = await d.translateText(
         sentence,
-        sourceLanguage as Language,
-        targetLanguage as Language,
+        sourceLanguageRemote as Language,
+        targetLanguageRemote as Language,
       );
       console.log("result", result);
       results[targetLanguage] = result.text;
