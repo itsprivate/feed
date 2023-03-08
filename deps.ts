a3a4c9c3d5d75b82251774e0273dbbfc1d5c13ee
Owen Young <theowenyoung@gmail.com>
2023-03-08 20:06:46 +0800

    add deps

--- deps.ts
+++ deps.ts
@@ -39,7 +39,7 @@ export { DigestClient } from "https://deno.land/x/digest_fetch@v1.2.1/mod.ts";
 export {
   DOMParser,
   Element,
-} from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
+} from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 export { retry } from "https://deno.land/x/retry@v2.0.0/mod.ts";
 export type {
   DOMParserMimeType,
@@ -53,19 +53,19 @@ export { S3, S3Bucket } from "./s3/mod.ts";
 // export { S3Client } from "../inbox/deno-s3-lite-client/mod.ts";
 
 // npm modules
-export { getMetadata } from "https://jspm.dev/page-metadata-parser@1.1.4";
+export { getMetadata } from "https://esm.sh/page-metadata-parser@1.1.4";
 // export { default as slug } from "https://jspm.dev/slug@6.0.0";
-import transliteration from "https://jspm.dev/transliteration@2.3.5";
+import { slugify } from "https://esm.sh/transliteration@2.3.5";
 // @ts-ignore: npm module
-const slug = transliteration.slugify;
+const slug = slugify;
 export { slug };
 export { default as kebabCase } from "https://jspm.dev/lodash@4.17.21/kebabCase";
 
 export { default as camelCase } from "https://jspm.dev/lodash@4.17.21/camelCase";
 export { default as groupBy } from "https://jspm.dev/lodash@4.17.21/groupBy";
-export { default as mustache } from "https://jspm.dev/mustache@4.2.0";
-export { default as jsonfeedToAtom } from "https://jspm.dev/jsonfeed-to-atom@1.2.2";
-export { default as jsonfeedToRSS } from "https://jspm.dev/jsonfeed-to-rss@3.0.6";
-export { default as tweetPatch } from "https://jspm.dev/tweet-patch@2.0.5";
+export { default as mustache } from "https://esm.sh/mustache@4.2.0";
+export { default as jsonfeedToAtom } from "https://esm.sh/jsonfeed-to-atom@1.2.2";
+export { default as jsonfeedToRSS } from "https://esm.sh/jsonfeed-to-rss@3.0.6";
+export { default as tweetPatch } from "https://esm.sh/tweet-patch@2.0.5";
 
-export { parse as parseXml } from "https://deno.land/x/xml@2.0.4/mod.ts";
+export { parse as parseXml } from "https://deno.land/x/xml@2.1.0/mod.ts";
