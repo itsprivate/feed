d2dbc658aac7f0854227601b6717ac98453ca713
Owen Young <theowenyoung@gmail.com>
2023-03-10 12:54:00 +0800

    add dp test

--- dp_test.ts
+++ dp_test.ts
@@ -1,11 +1,11 @@
 import { Deepl as DPro } from "https://esm.sh/immersive-translate@1.0.8";
 // load env
 import "https://deno.land/x/dotenv/load.ts";
-const d = new DPro(Deno.env.get("IM_DEEPL_AUTH_KEY"));
-
-const result = await d.translateText(
-  "hello world",
-  "EN",
-  "ZH",
-);
-console.log("result", result);
+// const d = new DPro(Deno.env.get("IM_DEEPL_AUTH_KEY"));
+//
+// const result = await d.translateText(
+//   "hello world",
+//   "EN",
+//   "ZH",
+// );
+// console.log("result", result);
